98ecbcc9dcc787d305e2c5010e01d97544376190
Amir <amir@quickode.com>
2020-09-01 20:37:22 +0300

    extracted function createTopLine

--- matching_lines.cpp
+++ matching_lines.cpp
@@ -7,203 +7,216 @@ using namespace cv;
 using namespace std;
 using namespace cv::separableFundamentalMatrix;
 
+Mat createHeatmap(InputArray ptsImg1, InputArray ptsImg2, const vector<line_info> &lineInfosImg1, const vector<line_info> &lineInfosImg2)
+{
+    /*
+    pts_lines         = np.zeros((len(pts1), len(lines_info_img1), len(lines_info_img2)))
 
-        Mat createHeatmap(InputArray ptsImg1, InputArray ptsImg2, const vector<line_info> &lineInfosImg1, const vector<line_info> &lineInfosImg2)
+    set_ix_1=set_shared_index_1(pts_lines)
+    [set_ix_1(x['matching_index'],x['line_index']) for x in lines_info_img1]
+    set_ix_2=set_shared_index_2(pts_lines)
+    [set_ix_2(x['matching_index'][0],x['line_index']) for x in lines_info_img2 if len(x['matching_index'][0])>0]
+
+    */
+
+    // Create heatmap
+    const int heatmapSize[] = { (int)lineInfosImg1.size(), (int)lineInfosImg2.size(), ptsImg1.size().height };
+    Mat heatmap = Mat::zeros(3, heatmapSize, CV_8U);
+
+    // Fill by first lines
+    for (auto &lineInfo : lineInfosImg1)
+    {
+        for (const int point_index : lineInfo.matching_indexes)
         {
-            /*
-            pts_lines         = np.zeros((len(pts1), len(lines_info_img1), len(lines_info_img2)))
-
-            set_ix_1=set_shared_index_1(pts_lines)
-            [set_ix_1(x['matching_index'],x['line_index']) for x in lines_info_img1]
-            set_ix_2=set_shared_index_2(pts_lines)
-            [set_ix_2(x['matching_index'][0],x['line_index']) for x in lines_info_img2 if len(x['matching_index'][0])>0]
-
-            */
-
-            // Create heatmap
-            const int heatmapSize[] = { (int)lineInfosImg1.size(), (int)lineInfosImg2.size(), ptsImg1.size().height };
-            Mat heatmap = Mat::zeros(3, heatmapSize, CV_8U);
-
-            // Fill by first lines
-            for (auto &lineInfo : lineInfosImg1)
+            for (int i = 0; i < heatmapSize[1]; i++)
             {
-                for (const int point_index : lineInfo.matching_indexes)
-                {
-                    for (int i = 0; i < heatmapSize[1]; i++)
-                    {
-                        heatmap.at<uchar>(lineInfo.line_index, i, point_index) = 1;
-                    }
-                }
+                heatmap.at<uchar>(lineInfo.line_index, i, point_index) = 1;
             }
+        }
+    }
 
-            // Fill by second lines
-            for (auto &lineInfo : lineInfosImg2)
+    // Fill by second lines
+    for (auto &lineInfo : lineInfosImg2)
+    {
+        for (const int point_index : lineInfo.matching_indexes)
+        {
+            for (int i = 0; i < heatmapSize[0]; i++)
             {
-                for (const int point_index : lineInfo.matching_indexes)
-                {
-                    for (int i = 0; i < heatmapSize[0]; i++)
-                    {
-                        heatmap.at<uchar>(i, lineInfo.line_index, point_index) += 1;
-                    }
-                }
+                heatmap.at<uchar>(i, lineInfo.line_index, point_index) += 1;
             }
-
-            return heatmap;
         }
+    }
 
-        template <typename _Tp>
-        vector<Point_<_Tp>> projectPointsOnLineByInices(InputArray _pts, const line_info &lineInfo, vector<int> indices)
-        {
-            vector<Point_<_Tp>> filteredPts;
-            filteredPts = byIndices<_Tp>(_pts, indices);
-            return projectPointsOnLine(lineInfo.bottom_left_edge_point, lineInfo.top_right_edge_point, filteredPts);
-        }
+    return heatmap;
+}
 
-        template <typename _Tp>
-        vector<int> uniqueIntersectedPoints(const vector<Point_<_Tp>> &matchingPoints1, const vector<Point_<_Tp>> &matchingPoints2)
-        {
-            vector<size_t> uniqueIdx1 = index_unique(vector<Point>(matchingPoints1.begin(), matchingPoints1.end()));
-            vector<size_t> uniqueIdx2 = index_unique(vector<Point>(matchingPoints2.begin(), matchingPoints2.end()));
+template <typename _Tp>
+vector<Point_<_Tp>> projectPointsOnLineByInices(InputArray _pts, const line_info &lineInfo, vector<int> indices)
+{
+    vector<Point_<_Tp>> filteredPts;
+    filteredPts = byIndices<_Tp>(_pts, indices);
+    return projectPointsOnLine(lineInfo.bottom_left_edge_point, lineInfo.top_right_edge_point, filteredPts);
+}
 
-            vector<int> unique_idx;
-            intersect1d(uniqueIdx1.begin(), uniqueIdx1.end(), uniqueIdx2.begin(), uniqueIdx2.end(), back_inserter(unique_idx));
+template <typename _Tp>
+vector<int> uniqueIntersectedPoints(const vector<Point_<_Tp>> &matchingPoints1, const vector<Point_<_Tp>> &matchingPoints2)
+{
+    vector<size_t> uniqueIdx1 = index_unique(vector<Point>(matchingPoints1.begin(), matchingPoints1.end()));
+    vector<size_t> uniqueIdx2 = index_unique(vector<Point>(matchingPoints2.begin(), matchingPoints2.end()));
 
-            return unique_idx;
-        }
+    vector<int> unique_idx;
+    intersect1d(uniqueIdx1.begin(), uniqueIdx1.end(), uniqueIdx2.begin(), uniqueIdx2.end(), back_inserter(unique_idx));
 
-        array<top_line,2> topTwoLinesWithMaxAngle(const vector<line_info> &lineInfosImg1, vector<top_line> &topLines)
-        {
-            sort(topLines.begin(), topLines.end(),
-                [](const top_line &line1, const top_line &line2) { return line1.min_dist < line2.min_dist; });
+    return unique_idx;
+}
 
-            const top_line firstLine = topLines[0];
+array<top_line,2> topTwoLinesWithMaxAngle(const vector<line_info> &lineInfosImg1, vector<top_line> &topLines)
+{
+    sort(topLines.begin(), topLines.end(),
+        [](const top_line &line1, const top_line &line2) { return line1.min_dist > line2.min_dist; });
+
+    const top_line firstLine = topLines[0];
             
-            auto firstLineEq = lineInfosImg1[firstLine.line1_index].line_eq_abc_norm;
-            int lineCount = min(20, (int)topLines.size());
-            double maxAngle = -180;
-            int maxAngleIx;
-            for (int i = 1; i < lineCount; i++)
-            {
-                auto lineEq = lineInfosImg1[topLines[i].line1_index].line_eq_abc_norm;
-                double angle = std::acosf(std::min<double>((lineEq.x*firstLineEq.x) + (lineEq.y*firstLineEq.y), 1)) * 180/ CV_PI;
-                if (std::min(angle, 180-angle) > maxAngle)
-                {
-                    maxAngle = std::min(angle, 180-angle);
-                    maxAngleIx = i;
-                }
-            }
-            return { move(firstLine), move(topLines[maxAngleIx]) };
-        }
-
-        vector<top_line> getTopMatchingLines(InputArray _ptsImg1, InputArray _ptsImg2, const vector<line_info> &lineInfosImg1,
-            const vector<line_info> &lineInfosImg2, int minSharedPoints, double inlierRatio)
+    auto firstLineEq = lineInfosImg1[firstLine.line1_index].line_eq_abc_norm;
+    int lineCount = min(20, (int)topLines.size());
+    double maxAngle = -180;
+    int maxAngleIx;
+    for (int i = 1; i < lineCount; i++)
+    {
+        auto lineEq = lineInfosImg1[topLines[i].line1_index].line_eq_abc_norm;
+        double angle = std::acosf(std::min<double>((lineEq.x*firstLineEq.x) + (lineEq.y*firstLineEq.y), 1)) * 180/ CV_PI;
+        if (std::min(angle, 180-angle) > maxAngle)
         {
+            maxAngle = std::min(angle, 180-angle);
+            maxAngleIx = i;
+        }
+    }
+    return { move(firstLine), move(topLines[maxAngleIx]) };
+}
 
-            // Create a heatmap between points of each line
-            Mat heatmap = createHeatmap(_ptsImg1, _ptsImg2, lineInfosImg1, lineInfosImg2);
+top_line createTopLine(InputArray _ptsImg1, InputArray _ptsImg2, const vector<Point3i> &num_shared_points_vote, 
+    const vector<line_info> &lineInfosImg1, const vector<line_info> &lineInfosImg2, int n, int num_line_ransac_iterations)
+{
+    top_line curr;
+    int k = num_shared_points_vote[n].y;
+    int j = num_shared_points_vote[n].z;
 
-            // Remove all entries which does not have two matching lines (pts_lines[pts_lines<2] =0)
-            heatmap.setTo(0, heatmap < 2);
+    vector<int> arr_idx;
+    intersect1d(lineInfosImg1[k].matching_indexes.begin(), lineInfosImg1[k].matching_indexes.end(), 
+        lineInfosImg2[j].matching_indexes.begin(), lineInfosImg2[j].matching_indexes.end(), back_inserter(arr_idx));
 
-            // Sum across points' index, this gives us how many shared points for each pair of lines
-            Mat hough_pts;
-            reduceSum3d<uchar, int>(heatmap, hough_pts, (int)CV_32S);
+    vector<Point2d> matchingPoints1 = projectPointsOnLineByInices<double>(_ptsImg1, lineInfosImg1[k], arr_idx);
+    vector<Point2d> matchingPoints2 = projectPointsOnLineByInices<double>(_ptsImg2, lineInfosImg2[j], arr_idx);
 
-            // Use voting to find out which lines shares points
-            // Convert to a list where each entry is 1x3: the number of shared points for each pair of line and their indices
-            auto num_shared_points_vote = indices<int>(hough_pts);
+    vector<int> uniqueIdx = uniqueIntersectedPoints(matchingPoints1, matchingPoints2);
 
-            //  Delete all non-relevent entries: That have minSharedPoints for each side (multiply by two - one for left line and one for right line).
-            num_shared_points_vote.erase(
-                std::remove_if(num_shared_points_vote.begin(), num_shared_points_vote.end(), [minSharedPoints](const Point3i p) {
-                    return (bool)(p.x < minSharedPoints * 2);
-                    }), num_shared_points_vote.end()
-                        );
+    // We need at least four unique points
+    if (uniqueIdx.size() < 4)
+        return curr;
 
-            // Sort the entries (in reverse order)
-            // Note: could've sorted them by x only, but the python code sorted like that
-            //std::sort(num_shared_points_vote.begin(), num_shared_points_vote.end(), lexicographicalSort3d<int>);
-            std::sort(num_shared_points_vote.begin(), num_shared_points_vote.end(), 
-                [](Point3i &a, Point3i &b)
-                {
-                    return a.x > b.x || (a.x == b.x && a.y < b.y) || (a.x ==b.x && a.y==b.y && a.z < b.z);
-                });
+    // Filter
+    matchingPoints1 = byIndices<double>(matchingPoints1, uniqueIdx);
+    matchingPoints2 = byIndices<double>(matchingPoints2, uniqueIdx);
 
-            // For each matching points on the matching lines,
-            // project the shared points to be exactly on the line
-            // start with the lines that shared the highest number of points, so we can do top-N
-            // return a list index by the lines (k,j) with the projected points themself
-            int num_line_ransac_iterations = int((log(0.01) / log(1 - pow(inlierRatio, 5)))) + 1;
-            
-            vector<top_line> topLines;
-            std::mutex mtx;
+    // Find inliers, inlier_idx_homography - index of inliers of all the line points
+    auto matchingPoints = VecMatchingPoints<double>(matchingPoints1, matchingPoints2);
+    auto lineInliersResult = lineInliersRansac(num_line_ransac_iterations, matchingPoints);
 
-            // Go over the top lines with the most number of shared points, project the points, store by the matching indices of the pair of lines
-            int num_sorted_lines = min((int)num_shared_points_vote.size(), 50);
-            parallel_for_(Range(0, num_sorted_lines), [&](const Range& range) {
-                for (size_t n = range.start; n < range.end; n++)
-                {
-                    int k = num_shared_points_vote[n].y;
-                    int j = num_shared_points_vote[n].z;
+    if (lineInliersResult.inlierIndexes.size() < 4)
+        return curr;
 
-                    vector<int> arr_idx;
-                    intersect1d(lineInfosImg1[k].matching_indexes.begin(), lineInfosImg1[k].matching_indexes.end(), 
-                        lineInfosImg2[j].matching_indexes.begin(), lineInfosImg2[j].matching_indexes.end(), back_inserter(arr_idx));
-
-                    vector<Point2d> matchingPoints1 = projectPointsOnLineByInices<double>(_ptsImg1, lineInfosImg1[k], arr_idx);
-                    vector<Point2d> matchingPoints2 = projectPointsOnLineByInices<double>(_ptsImg2, lineInfosImg2[j], arr_idx);
-
-                    vector<int> uniqueIdx = uniqueIntersectedPoints(matchingPoints1, matchingPoints2);
-
-                    // We need at least four unique points
-                    if (uniqueIdx.size() < 4)
-                        continue;
-
-                    // Filter
-                    matchingPoints1 = byIndices<double>(matchingPoints1, uniqueIdx);
-                    matchingPoints2 = byIndices<double>(matchingPoints2, uniqueIdx);
-
-                    // Find inliers, inlier_idx_homography - index of inliers of all the line points
-                    auto matchingPoints = VecMatchingPoints<double>(matchingPoints1, matchingPoints2);
-                    auto lineInliersResult = lineInliersRansac(num_line_ransac_iterations, matchingPoints);
-
-                    if (lineInliersResult.inlierIndexes.size() < 4)
-                        continue;
-
-                    auto inlierPoints1 = byIndices<double>(matchingPoints1, lineInliersResult.inlierIndexes);
-                    auto inlierPoints2 = byIndices<double>(matchingPoints1, lineInliersResult.inlierIndexes);
+    auto inlierPoints1 = byIndices<double>(matchingPoints1, lineInliersResult.inlierIndexes);
+    auto inlierPoints2 = byIndices<double>(matchingPoints2, lineInliersResult.inlierIndexes);
                 
-                    auto endpoints = intervalEndpoints(inlierPoints1);
-                    auto median = intervalMedian(inlierPoints1, endpoints.firstIdx, endpoints.secondIdx);
-                
-                    top_line curr;
-                    curr.num_inliers = (int)lineInliersResult.inlierIndexes.size();
-                    curr.line_points_1 = inlierPoints1;
-                    curr.line_points_2 = inlierPoints2;
-                    curr.line1_index = k;
-                    curr.line2_index = j;
-                    curr.inlier_selected_index = { endpoints.firstIdx, endpoints.secondIdx, median.medianIdx };
-                    curr.selected_line_points1 = byIndices<double>(inlierPoints1, curr.inlier_selected_index);
-                    curr.selected_line_points2 = byIndices<double>(inlierPoints2, curr.inlier_selected_index);
-                    curr.max_dist = endpoints.distance;
-                    curr.min_dist = median.minDistance;
-                    curr.homg_err = lineInliersResult.meanError;
+    auto endpoints = intervalEndpoints(inlierPoints1);
+    auto midPointResult = intervalPointClosestToCenter(inlierPoints1, endpoints.firstIdx, endpoints.secondIdx);
 
-                    mtx.lock();
-                    topLines.push_back(curr);
-                    mtx.unlock();
+    curr.num_inliers = (int)lineInliersResult.inlierIndexes.size();
+    curr.line_points_1 = inlierPoints1;
+    curr.line_points_2 = inlierPoints2;
+    curr.line1_index = k;
+    curr.line2_index = j;
+    curr.inlier_selected_index = { endpoints.firstIdx, endpoints.secondIdx, midPointResult.midPointIdx };
+    curr.selected_line_points1 = byIndices<double>(inlierPoints1, curr.inlier_selected_index);
+    curr.selected_line_points2 = byIndices<double>(inlierPoints2, curr.inlier_selected_index);
+    curr.max_dist = endpoints.distance;
+    curr.min_dist = midPointResult.minDistance;
+    curr.homg_err = lineInliersResult.meanError;
 
-                }
-            });
-            
-            
-            if (topLines.size() < 2)
-                return {};
-            
-            auto topTwoLines = topTwoLinesWithMaxAngle(lineInfosImg1, topLines);
-            return { topTwoLines[0], topTwoLines[1] };
-            //return {};
+    return curr;
+}
+
+vector<top_line> getTopMatchingLines(InputArray _ptsImg1, InputArray _ptsImg2, const vector<line_info> &lineInfosImg1,
+    const vector<line_info> &lineInfosImg2, int minSharedPoints, double inlierRatio)
+{
+
+    // Create a heatmap between points of each line
+    Mat heatmap = createHeatmap(_ptsImg1, _ptsImg2, lineInfosImg1, lineInfosImg2);
+
+    // Remove all entries which does not have two matching lines (pts_lines[pts_lines<2] =0)
+    heatmap.setTo(0, heatmap < 2);
+
+    // Sum across points' index, this gives us how many shared points for each pair of lines
+    Mat hough_pts;
+    reduceSum3d<uchar, int>(heatmap, hough_pts, (int)CV_32S);
+
+    // Use voting to find out which lines shares points
+    // Convert to a list where each entry is 1x3: the number of shared points for each pair of line and their indices
+    auto num_shared_points_vote = indices<int>(hough_pts);
+
+    //  Delete all non-relevent entries: That have minSharedPoints for each side (multiply by two - one for left line and one for right line).
+    // Note: This could be done only on the x column but the python code does the same
+    num_shared_points_vote.erase(
+        std::remove_if(num_shared_points_vote.begin(), num_shared_points_vote.end(), [minSharedPoints](const Point3i p) {
+            return (bool)(p.x < minSharedPoints * 2 || p.y < minSharedPoints * 2 || p.z < minSharedPoints * 2);
+            }), num_shared_points_vote.end()
+                );
+
+    // Sort the entries (in reverse order)
+    // Note: could've sorted them by x only, but the python code sorted like that
+    //std::sort(num_shared_points_vote.rbegin(), num_shared_points_vote.rend(), lexicographicalSort3d<int>);
+    std::sort(num_shared_points_vote.begin(), num_shared_points_vote.end(), 
+        [](Point3i &a, Point3i &b)
+        {
+            return a.x > b.x || (a.x == b.x && a.y < b.y) || (a.x == b.x && a.y == b.y && a.z < b.z);
+        });
+
+    // For each matching points on the matching lines,
+    // project the shared points to be exactly on the line
+    // start with the lines that shared the highest number of points, so we can do top-N
+    // return a list index by the lines (k,j) with the projected points themself
+    int num_line_ransac_iterations = int((log(0.01) / log(1 - pow(inlierRatio, 5)))) + 1;
+    
+    // Go over the top lines with the most number of shared points, project the points, store by the matching indices of the pair of lines
+    int num_sorted_lines = min((int)num_shared_points_vote.size(), 50);
+    top_line topLines[50];
+    for (size_t n = 0; n < num_sorted_lines; n++)
+    {
+        topLines[n] = createTopLine(_ptsImg1, _ptsImg2, num_shared_points_vote, 
+            lineInfosImg1, lineInfosImg2, n, num_line_ransac_iterations);
+    }
+    /*parallel_for_(Range(0, num_sorted_lines), [&](const Range& range) {
+        for (size_t n = range.start; n < range.end; n++)
+        {
+            topLines[n] = createTopLine(_ptsImg1, _ptsImg2, num_shared_points_vote, 
+                lineInfosImg1, lineInfosImg2, n, num_line_ransac_iterations);
         }
+    });*/
+
+    vector<top_line> nonEmptyTopLines;
+    for (size_t i = 0; i < num_sorted_lines; i++)
+    {
+        if (!topLines[i].empty())
+            nonEmptyTopLines.push_back(topLines[i]);
+    }
+            
+    if (nonEmptyTopLines.size() < 2)
+        return {};
+            
+    auto topTwoLines = topTwoLinesWithMaxAngle(lineInfosImg1, nonEmptyTopLines);
+    return { topTwoLines[0], topTwoLines[1] };
+    //return {};
+}
 
         template <typename _Tp>
         vector<_Tp> findIntersectionPoints(_Tp rho, _Tp theta, const int im_size_w, const int im_size_h)
